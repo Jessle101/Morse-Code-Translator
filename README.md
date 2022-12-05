d25a75ee884dbf210d05278918b07cb1e4597e9f
Jessle101 <jessie.g.osti@gmail.com>
2022-12-05 14:24:58 +1030

    englishToMorse base function

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Morse-Code-Translator
+
 This program translates words and/or characters to Morse Code, vice versa!
+
+## To Do List
+
+- write morseToEnglish func.
+- creates individual function files
+- write tests for functions
+- validation of string: i.e. A - Z characters & morse code characters only (.\_)
+- catch, throw error
+- test cases for what the functions achieve
--- script.js
+++ script.js
@@ -7,47 +7,92 @@ let translation = "Translation";
 const input = document.querySelector("#input");
 const output = document.querySelector("#output");
 
+// A to Z in Morse Code
+const morseCode = {
+  A: ".-",
+  B: "-...",
+  C: "-.-.",
+  D: "-..",
+  E: ".",
+  F: "..-.",
+  G: "--.",
+  H: "....",
+  I: "..",
+  J: ".---",
+  K: "-.-",
+  L: ".-..",
+  M: "--",
+  N: "-.",
+  O: "---",
+  P: ".--.",
+  Q: "--.-",
+  R: ".-.",
+  S: "...",
+  T: "-",
+  U: "..-",
+  W: ".--",
+  X: "-..-",
+  Y: "-.--",
+  Z: "--..",
+};
+
+// functions to translate
+const englishToMorse = (engStr, obj) => {
+  const engStr2 = engStr.toUpperCase();
+  const lettersArr = engStr2.split("");
+  // takes an anonymous function or function definition
+  const morseArr = lettersArr.map((engStr) => {
+    return obj[engStr];
+  });
+  const morseStr = morseArr.join(" ");
+  console.log(morseStr);
+  return morseStr;
+};
+
+// instead of object value, add object key & add a specified value (reverse it)
+const morseToEnglish = () => {};
+
 translate = {
-  englishToMorse: "true",
+  englishToMorse: true,
   input: "",
   output: "",
 };
 
-let e2m = translate.englishToMorse;
+const e2m = translate.englishToMorse;
 let inputArr = 0;
 let outputArr = 0;
+
+input.addEventListener("input", function () {
+  translate.input = input.value;
+
+  // if translating english to morse
+  if (e2m === true) {
+    const something = englishToMorse(translate.input, morseCode);
+    output.innerHTML = something;
+    console.log(something);
+    // if translating morse to english
+  } else {
+    // morseToEnglish(translate.input, morseCode);
+  }
+});
 
 transSwitch.addEventListener("click", function () {
   if (hasSwitched === false) {
     text1.innerHTML = "Morse Code";
     text2.innerHTML = "English";
+    input.value = output.innerHTML;
     hasSwitched = true;
+    translate.englishToMorse = false;
   } else if (hasSwitched === true) {
     text1.innerHTML = "English";
     text2.innerHTML = "Morse Code";
+    input.value = output.innerHTML;
     hasSwitched = false;
+    translate.englishToMorse = true;
   }
 });
 
-input.addEventListener("input", function () {
-  translate.input = input.value;
-  output.innerHTML = translate.input;
-  console.log(translate);
+englishToMorse("hello", morseCode);
 
-  inputArr = translate.input.split("");
-  console.log(inputArr);
-
-  // if translating english to morse
-  if (e2m === "true") {
-    // if translating morse to english
-  } else {
-  }
-});
-
-// function to translate
-const translateIt = () => {
-  if (e2m === true) {
-    inputArr.filter();
-  } else {
-  }
-};
+// file for each m2e, etc functions
+// JS tests, i.e.
